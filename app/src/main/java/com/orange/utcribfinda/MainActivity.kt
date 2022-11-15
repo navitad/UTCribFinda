453e250d9f7e5ce67926b0b4b3aa06006ba6a501
Moe Ghanem <moeghanem@utexas.edu>
2022-11-14 18:03:33 -0600

    cleaned up layout ish for home

--- app/src/main/java/com/orange/utcribfinda/MainActivity.kt
+++ app/src/main/java/com/orange/utcribfinda/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.orange.utcribfinda
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+//import androidx.navigation.ui.AppBarConfiguration
+//import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.orange.utcribfinda.databinding.ActivityMainBinding
 
@@ -22,14 +23,20 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_saved
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // set icon for the bar
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setIcon(R.mipmap.ic_launcher_round)
+//        method below changes name based on fragment
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_saved
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
+
 }
