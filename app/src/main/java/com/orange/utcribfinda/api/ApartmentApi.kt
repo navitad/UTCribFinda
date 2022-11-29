06bafcd8c8b49c75015f26afa39e1762d4d0292e
navitadhillon <73406046+navitadhillon@users.noreply.github.com>
2022-11-28 18:36:24 -0600

    api really nice and pretty

---
+++ app/src/main/java/com/orange/utcribfinda/api/ApartmentApi.kt
@@ -0,0 +1,67 @@
+package com.orange.utcribfinda.api
+
+import android.text.SpannableString
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import edu.cs371m.reddit.api.ListingPost
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.lang.reflect.Type
+
+interface ApartmentApi {
+    @GET("https://wrapapi.com/use/navita/apartments/villasOnRioListings/0.0.1?wrapAPIKey=MVBCXaViCKCM9UnqmwuUo3d0R9fTftzN")
+    suspend fun getVillasOnRioPosts() : ListingResponse
+
+    class ListingResponse(val data: ListingData)
+
+    class ListingData(
+        val children: List<ListingChildrenResponse>,
+        val after: String?,
+        val before: String?
+    )
+    data class ListingChildrenResponse(val data: ListingPost)
+
+    class SpannableDeserializer : JsonDeserializer<SpannableString> {
+        // @Throws(JsonParseException::class)
+        override fun deserialize(
+            json: JsonElement,
+            typeOfT: Type,
+            context: JsonDeserializationContext
+        ): SpannableString {
+            return SpannableString(json.asString)
+        }
+    }
+
+    companion object {
+        // Leave this as a simple, base URL.  That way, we can have many different
+        // functions (above) that access different "paths" on this server
+        // https://square.github.io/okhttp/4.x/okhttp/okhttp3/-http-url/
+        var url = HttpUrl.Builder()
+            .scheme("https")
+            .host("wrapapi.com")
+            .build()
+
+        // Public create function that ties together building the base
+        // URL and the private create function that initializes Retrofit
+        fun create(): ApartmentApi = create(url)
+        private fun create(httpUrl: HttpUrl): ApartmentApi {
+            val client = OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    this.level = HttpLoggingInterceptor.Level.BASIC
+                })
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(httpUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApartmentApi::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/orange/utcribfinda/api/ListingPostRepository.kt
@@ -0,0 +1,50 @@
+package com.orange.utcribfinda.api
+
+import android.text.SpannableString
+import com.google.gson.GsonBuilder
+import com.orange.utcribfinda.MainActivity
+import edu.cs371m.reddit.api.ListingPost
+
+class ListingPostRepository(private val apartmentApi: ApartmentApi) {
+    // NB: This is for our testing.
+    val gson = GsonBuilder().registerTypeAdapter(
+        SpannableString::class.java, ApartmentApi.SpannableDeserializer()
+    ).create()
+
+    private fun unpackPosts(response: ApartmentApi.ListingResponse): List<ListingPost> {
+        // XXX Write me.
+        val result = mutableListOf<ListingPost>()
+        val children = response.data.children
+        for(i in 0..children.size-1) {
+            result.add(children[i].data)
+        }
+        return result
+
+    }
+
+    suspend fun getPosts(subreddit: String): List<ListingPost> {
+//        if (MainActivity.globalDebug) {
+//            val response = gson.fromJson(
+//                MainActivity.jsonAww100,
+//                ApartmentApi.ListingResponse::class.java)
+//            return unpackPosts(response)
+//        } else {
+            // XXX Write me.
+            val response = apartmentApi.getVillasOnRioPosts()
+            return unpackPosts(response)
+//        }
+    }
+
+//    suspend fun getSubreddits(): List<ListingPost> {
+//        if (MainActivity.globalDebug) {
+//            val response = gson.fromJson(
+//                MainActivity.subreddit1,
+//                ApartmentApi.ListingResponse::class.java)
+//            return unpackPosts(response)
+//        } else {
+//            // XXX Write me.
+//            val response = ApartmentApi.getSubreddit()
+//            return unpackPosts(response)
+//        }
+//    }
+}
